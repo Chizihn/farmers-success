2272ddf82583262e0d04abc32c2b7c0dc04869ce
Ehceyn <itzmeruz31@gmail.com>
2024-10-08 15:14:00 +0100

    fix git error

---
+++ app/farm/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+const RedirectToFarmsPage = () => {
+  const router = useRouter();
+  //
+  useEffect(() => {
+    router.push("/farms");
+  }, [router]);
+
+  return <p>Redirecting...</p>;
+};
+
+export default RedirectToFarmsPage;
